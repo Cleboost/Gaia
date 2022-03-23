a9481cec9497827a09c5594e9571b47b7e89d24f
Cleboost <clement.balarot@gmail.com>
2022-03-23 14:45:19 +0100

    Compatibilité plusieurs lang

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ module.exports.start = () => {
   if (!existsSync('./config_api.json')) {
     //sinon le creer
     writeFileSync('./config_api.json', JSON.stringify(require('./ex_config.json')));
-    return console.log('Remplissez le fichier "config_api.json" !!');
+    return console.log(t_erreur["fr"].Efile+"config_api.json !!");
     //writeFileSync('./api-sans-nom/', JSON.stringify(_data));
   };
   const config = require('./../config_api.json');
   if (t_main.lang.indexOf(config.lang)===-1) {
     return console.log("Merci de définire un langue existante "+t_main.lang)
   };
+  let lang=config.lang;
+  
   //recupere les donnees de connection
   let con = mysql.createConnection({
     host: config.db.host,
@@ -27,7 +29,7 @@ module.exports.start = () => {
 //connect @con à la bdd
   con.connect(function(err) {
     if (err) throw err;
-    if (config.log.connect) console.log("Connected to "+config.db.database+" on "+config.db.host);
+    if (config.log.connect) console.log(t_main[lang].connect[0]+config.db.database+t_main[lang].connect[1]+config.db.host);//msg de connection
     module.exports = con;
   });
 };
