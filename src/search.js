da4e2224a4ad6be7254014349244103ae0776f16
Cleboost <clement.balarot@gmail.com>
2022-03-31 18:29:35 +0200

    Organisation fonction
    On regroupe certaines fonction

---
+++ src/search.js
@@ -0,0 +1,27 @@
+//Fonction pour rechercher un @element pour @element == @data dans @table
+module.exports.serachByElement = (element, data, table) => {
+    let i = 0;
+    try {
+        do {
+            if (table[i][element] === data) return i;
+            i++;
+        } while (i < table.lenth)
+        console.log('Pas de donnée pour l\'element "' + element + '" = "' + data + '" !');
+    } catch (err) {
+        throw 'L\'element ' + element + ' n\'existe pas !';
+    }
+}
+//Fonction pour rechercher un @element pour @element == @data dans @table
+/*module.exports.serachByElement = (element, data, table) => {
+    let i = 0;
+    try {
+        do {
+            if (table[i][element] === data) return i;
+            i++;
+        } while (i < table.lenth)
+        console.log('Pas de donnée pour l\'element "' + element + '" = "' + data + '" !');
+    } catch (err) {
+        throw 'L\'element ' + element + ' n\'existe pas !';
+    }
+}*/
+
