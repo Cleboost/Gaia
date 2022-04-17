a012df06b8ec2af476233f063d99cd3922cd7acb
Youritch <86657843+Youritch@users.noreply.github.com>
2022-04-17 19:26:42 +0200

    cleaning

--- src/search.js
+++ src/search.js
@@ -11,17 +11,3 @@ module.exports.serachByElement = (element, data, table) => {
         throw 'L\'element ' + element + ' n\'existe pas !';
     }
 }
-//Fonction pour rechercher un @element pour @element == @data dans @table
-/*module.exports.serachByElement = (element, data, table) => {
-    let i = 0;
-    try {
-        do {
-            if (table[i][element] === data) return i;
-            i++;
-        } while (i < table.lenth)
-        console.log('Pas de donnée pour l\'element "' + element + '" = "' + data + '" !');
-    } catch (err) {
-        throw 'L\'element ' + element + ' n\'existe pas !';
-    }
-}*/
-
