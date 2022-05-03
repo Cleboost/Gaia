395320ce9f5c47489f44c8bda72c3489bee606e9
Youritch <86657843+Youritch@users.noreply.github.com>
2022-05-03 20:50:29 +0200

    renomer en save.js

--- src/saveByTable.js
+++
@@ -1,6 +0,0 @@
-const con = require("..")
-//c'est pour quoi ?
-module.exports.saveByTable = (indexUser, data) => {
-    con.query("UPDATE `compte_user` SET `id`='',`discord_id`='',`data`='"+data+"' WHERE "+indexUser);
-}
-//ok
