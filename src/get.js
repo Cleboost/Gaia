7d29f8faa0add0334df2fa36cdc4c5aa9e89dc68
Cleboost <clement.balarot@gmail.com>
2022-05-09 20:38:29 +0200

    correct import index.js

--- src/get.js
+++ src/get.js
@@ -1,7 +1,7 @@
-const con = require('./../index.json').con;
+const con = require('./../index.js').con;
 //fonction pour recup une @table de la bdd
 module.exports.table = (table) => {
-    con.query("SELECT * FROM "+table+" ORDER BY id DESC", function (err, result, fields) {
+    con.query("SELECT * FROM " + table + " ORDER BY id DESC", function (err, result, fields) {
         if (err) throw err;
         return result;
     });
