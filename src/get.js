7f0b069fb9c29d170d6988122b01dbfdbc299862
Youritch <86657843+Youritch@users.noreply.github.com>
2022-05-20 12:01:18 +0200

    mmodification initialization

--- src/get.js
+++ src/get.js
@@ -1,4 +1,14 @@
-const con = require('./../index.js').con;
+//initialisation :
+const c = require('./../../config_gaia.json');
+const er = require('./../texte/erreur.json');
+const mysql = require('mysql');
+const m = require('./../texte/main.json');
+con = mysql.createConnection({
+    host: c.db.host,
+    user: c.db.user,
+    password: c.db.password,
+    database: c.db.database
+});
 //fonction pour recup une @table de la bdd
 module.exports.table = (table) => {
     con.query("SELECT * FROM " + table + " ORDER BY id DESC", function (err, result, fields) {
--- src/save.js
+++ src/save.js
@@ -1,4 +1,14 @@
-const con = require('./../index.js').con;
+//initialisation :
+const c = require('./../../config_gaia.json');
+const er = require('./../texte/erreur.json');
+const mysql = require('mysql');
+const m = require('./../texte/main.json');
+con = mysql.createConnection({
+    host: c.db.host,
+    user: c.db.user,
+    password: c.db.password,
+    database: c.db.database
+});
 //c'est pour quoi ?
 module.exports.saveByID = (table, id, data, colonne) => {
   con.query("UPDATE " + table + " SET " + colonne + " = '" + data + "' WHERE id = '" + id + "'", function (err, result) {
