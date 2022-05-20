200ca3fe93c109734cd528a3497df3b855972c3c
Youritch <86657843+Youritch@users.noreply.github.com>
2022-05-20 12:04:12 +0200

    ajout conection à la bdd

--- src/get.js
+++ src/get.js
@@ -3,7 +3,7 @@ const c = require('./../../config_gaia.json');
 const er = require('./../texte/erreur.json');
 const mysql = require('mysql');
 const m = require('./../texte/main.json');
-con = mysql.createConnection({
+let con = mysql.createConnection({
     host: c.db.host,
     user: c.db.user,
     password: c.db.password,
@@ -11,6 +11,11 @@ con = mysql.createConnection({
 });
 //fonction pour recup une @table de la bdd
 module.exports.table = (table) => {
+    //connect @con à la bdd
+    con.connect(function (err) {
+        if (err) return console.log(er[c.lang].EconDB);
+        if (c.log.connect) console.log(m[c.lang].connect[0] + c.db.database + m[c.lang].connect[1] + c.db.host);//msg de connection
+    });
     con.query("SELECT * FROM " + table + " ORDER BY id DESC", function (err, result, fields) {
         if (err) throw err;
         return result;
